ce8188820bee1b31cbe9afe71dbf326e29da8080
Igor <ipankrat85@gmail.com>
2026-06-08 22:12:42 +0300

    Add test, mocha, chai

---
+++ src/js/test.js
@@ -0,0 +1,16 @@
+//Тестируем функцию таймера
+describe("Таймер", function() {
+    it("Возвращает ли функция объект", function() {
+        assert.typeOf(getTimeRemaining(), "object")
+    });
+
+    it("Устанавливаем таймер обратного отсчёта", function() {
+        assert.typeOf(setClock("timer", deadline), "string");
+    });
+
+    describe("Общая сумма", function() {
+        it("Изначально равен 0", function() {
+            assert.equal(total, 0);
+        })
+    })
+})
